de9996ff252a10fcc7fb0cd2bcaaa1f657fce527
Alex <stefanalexb@gmail.com>
2019-02-18 16:25:47 -0500

    Version 2 with UART communcation up and running

---
+++ AliceRobot2.0.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=AliceRobot2.0.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/AliceRobot2.0.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=alicerobot2.0.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/alicerobot2.0.x.tar
